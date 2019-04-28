ebc9fe2aa0a3a3252e5ebdefbe49c90a0e36b2a8
Ray Luxembourg <rayluxembourg@gmail.com>
2019-04-28 14:36:50 +0300

    Update package json and cors middleware usage

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,11 @@ import express, {RequestHandler} from "express"
 import helmet from "helmet"
 import {urlencoded, json} from "body-parser"
 import {routing} from "./routing";
+import cors from "cors";
 
 const app = express()
 
+app.use(cors())
 app.use(helmet())
 
 app.use(urlencoded({extended: false}))
